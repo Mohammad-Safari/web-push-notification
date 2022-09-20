0ba47629c0886c44e2783a506b11b9d3b5d6995a
M.safari <m.safari@Tosan.com>
2022-09-20 12:08:43 +0430

    project initialized

---
+++ web-module/src/main/java/com/my/mvc/project/mymvcproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.my.mvc.project.mymvcproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyMvcProjectApplication.class);
+	}
+
+}
