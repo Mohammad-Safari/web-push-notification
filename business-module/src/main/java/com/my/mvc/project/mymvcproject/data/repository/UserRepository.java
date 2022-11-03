6d959018029e9f85e7a72d120ffe2cf2fc854b66
Mohammad Safari <msafari8051@gmail.com>
2022-11-04 00:23:55 +0330

    spring security persistent utils

--- business-module/src/main/java/com/my/mvc/project/mymvcproject/data/repository/UserRepository.java
+++ business-module/src/main/java/com/my/mvc/project/mymvcproject/data/repository/UserRepository.java
@@ -1,12 +1,18 @@
 package com.my.mvc.project.mymvcproject.data.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.my.mvc.project.mymvcproject.model.User;
+import com.my.mvc.project.mymvcproject.model.UserDetails;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByDetails(UserDetails details);
 
-    Optional<User> findByEmail(String email);
+    @Query("select u from User u where u.details = ?1")
+    List<User> findByUserName(String userName);
 }
