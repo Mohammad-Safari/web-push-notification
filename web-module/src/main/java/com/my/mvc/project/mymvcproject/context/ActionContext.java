c135de47cad84d0cff47812b2962216690f86d3f
M.safari <m.safari@Tosan.com>
2022-11-05 20:26:34 +0330

    custom application context and auth filters

---
+++ web-module/src/main/java/com/my/mvc/project/mymvcproject/context/ActionContext.java
@@ -0,0 +1,18 @@
+package com.my.mvc.project.mymvcproject.context;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class ActionContext {
+    String name;
+    String state;
+
+}
--- web-module/src/main/java/com/my/mvc/project/mymvcproject/controller/api/AuthController.java
+++ web-module/src/main/java/com/my/mvc/project/mymvcproject/controller/api/AuthController.java
@@ -1,18 +1,25 @@
 package com.my.mvc.project.mymvcproject.controller.api;
 
+import java.util.Base64;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.mvc.project.mymvcproject.context.UserContext;
 import com.my.mvc.project.mymvcproject.dto.LoginDto;
 import com.my.mvc.project.mymvcproject.dto.SignupDto;
-import com.my.mvc.project.mymvcproject.model.User;
+import com.my.mvc.project.mymvcproject.filter.ContextConstants;
 import com.my.mvc.project.mymvcproject.service.UserDetailsService;
 import com.my.mvc.project.mymvcproject.service.UserService;
 
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 
 @RestController()
 @RequestMapping("/api")
@@ -22,7 +29,12 @@ public class AuthController {
     UserDetailsService detailsService;
 
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto) {
+    @SneakyThrows
+    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response, UserContext userContext) {
+        userContext.setUsername(loginDto.getUsername());
+        var a = new ObjectMapper().writeValueAsString(userContext);
+        response.setHeader(ContextConstants.AUTHORIZATION_HEADER_NAME,
+                ContextConstants.AUTHORIZATION_HEADER_TYPE + " " + Base64.getEncoder().encodeToString(a.getBytes()));
         return "";
     }
 
