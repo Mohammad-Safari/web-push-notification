949a4968696dea57b34666e592c44df2aaca6509
M.safari <m.safari@Tosan.com>
2022-11-09 16:13:07 +0330

    client event serveice is implemented

--- web-module/src/main/resources/static/script/page.js
+++ web-module/src/main/resources/static/script/page.js
@@ -1,18 +1,40 @@
-const eventSource = new EventSource("http://localhost:8080/event/sse", {});
+const rbeCounter = 2;
+const eventSource = new EventSource("/event/sse/2", {});
+const eventSource2 = new EventSource(`/event/rbe?counter=${rbeCounter}`, {});
 
 window.onload = (_) => {
   const eventList = document.getElementById("list");
   eventSource.onopen = (event) => {
-    const newElement = document.createElement("li");
-    newElement.textContent = `open: ${event.data}`;
-    eventList.appendChild(newElement);
+    addEntry(event, eventList, `open: ${event.target.url}`);
   };
   eventSource.addEventListener("sse event - mvc", (event) => {
-    const newElement = document.createElement("li");
-    newElement.textContent = `message: ${event.data}`;
-    eventList.appendChild(newElement);
+    addEntry(event, eventList, `event: sse event - mvc; mvc: ${event.data}`);
   });
+  eventSource.addEventListener("complete", (event) => {
+    addEntry(event, eventList, `event: complete; data: ${event.data}`);
+    eventSource.close();
+  });
+  /* ************************************************* */
+  const eventList2 = document.getElementById("list2");
+  let rbeCounterCopy = rbeCounter;
+  eventSource2.onopen = (event) => {
+    addEntry(event, eventList2, `open: ${event.target.url}`);
+  };
+  eventSource2.onmessage = (event) => {
+    rbeCounterCopy--;
+    addEntry(event, eventList2, `message: ${event.data}`);
+    if (rbeCounterCopy === 0) {
+      eventSource2.close();
+      addEntry(event, eventList2, "connection is closed");
+    }
+  };
   window.onunload = (_) => {
     eventSource.close();
+    eventSource2.close();
   };
 };
+function addEntry(event, eventList, textFormat) {
+  const newElement = document.createElement("li");
+  newElement.textContent = textFormat;
+  eventList.appendChild(newElement);
+}
