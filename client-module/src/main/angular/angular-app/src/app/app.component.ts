dbaf7180ef8413fe0d6aac061c58f87b9ee33698
M.safari <m.safari@Tosan.com>
2022-11-20 11:34:35 +0330

    auth guard added

--- client-module/src/main/angular/angular-app/src/app/app.component.ts
+++ client-module/src/main/angular/angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './service/login/login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Spring Boot - Angular Application';
+  name: string | null;
+  constructor(private loginService: LoginService) {}
+  ngOnInit(): void {
+    this.loginService.isAuthenticated()
+      ? this.loginService.getUsername()
+      : false;
+    this.loginService.loginObservable.subscribe((name) => {
+      this.name = name;
+    });
+  }
 }
--- client-module/src/main/angular/angular-app/src/app/service/login/login.service.ts
+++ client-module/src/main/angular/angular-app/src/app/service/login/login.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { LoginModel } from '../../model/login-model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
+  public loginObservable = new BehaviorSubject<string | null>(null);
+  private _isAuthenticated = false;
   constructor(private httpClient: HttpClient) {}
   login(loginModel: LoginModel) {
     return this.httpClient
@@ -19,7 +22,45 @@ export class LoginService {
               data.body['Authorization'] ??
               ''
           );
+        }),
+        tap(() => {
+          this._isAuthenticated = true;
+          this.getUsername();
         })
       );
+  }
+
+  logout() {
+    return this.httpClient
+      .get('/api/logout', { observe: 'response' })
+      .pipe(
+        map((data: HttpResponse<any>) => {
+          localStorage.removeItem('Authorization');
+        }),
+        tap(() => {
+          this._isAuthenticated = false;
+          this.loginObservable.next(null);
+        })
+      );
+  }
+
+  public isAuthenticated(): boolean {
+    return (
+      this._isAuthenticated || localStorage.getItem('Authorization') != null
+    );
+  }
+
+  public getUsername(): string {
+    if (!this.isAuthenticated()) {
+      throw Error('user is not authenticated');
+    }
+    let token = localStorage.getItem('Authorization') ?? '';
+    token = token?.substring('CUSTOM-AUTH '.length);
+    if (!token || !JSON.parse(atob(token))) {
+      throw Error('token is corrupted');
+    }
+    const username = JSON.parse(atob(token))['username'];
+    this.loginObservable.next(username);
+    return username;
   }
 }
