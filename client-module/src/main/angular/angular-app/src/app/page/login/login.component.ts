72d85db925abe78614cf6e98556c4db9057d281a
M.safari <m.safari@Tosan.com>
2022-11-25 13:08:16 +0330

    component observables refactored

--- client-module/src/main/angular/angular-app/src/app/page/login/login.component.ts
+++ client-module/src/main/angular/angular-app/src/app/page/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { LoginModel } from 'src/app/model/login-model';
 import { LoginService } from 'src/app/service/login/login.service';
 
@@ -10,19 +12,35 @@ import { LoginService } from 'src/app/service/login/login.service';
 })
 export class LoginComponent implements OnInit {
   public loginModel: LoginModel = new LoginModel();
+  private _loginUnsubscribed: Subject<any> = new Subject();
 
   constructor(private loginService: LoginService, private router: Router) {}
 
   ngOnInit(): void {}
 
   onSubmit() {
-    this.loginService.login(this.loginModel).subscribe({
-      next: () => {
-        this.router.navigate(['../']);
-      },
-    });
+    this.loginService
+      .login(this.loginModel)
+      .pipe(takeUntil(this._loginUnsubscribed))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['../']);
+        },
+      });
   }
   logout() {
-    this.loginService.logout().subscribe();
+    this.loginService
+      .logout()
+      .pipe(takeUntil(this._loginUnsubscribed))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['../']);
+        },
+      });
+  }
+
+  ngOnDestroy() {
+    this._loginUnsubscribed.next();
+    this._loginUnsubscribed.complete();
   }
 }
