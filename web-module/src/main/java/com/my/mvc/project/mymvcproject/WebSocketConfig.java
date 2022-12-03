082267d2b13cee39c8554704dffedd9832d11aaa
Mohammad Safari <msafari8051@gmail.com>
2022-12-03 03:55:32 +0330

    user scope socket notification

--- web-module/src/main/java/com/my/mvc/project/mymvcproject/WebSocketConfig.java
+++ web-module/src/main/java/com/my/mvc/project/mymvcproject/WebSocketConfig.java
@@ -1,24 +1,35 @@
 package com.my.mvc.project.mymvcproject;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import com.my.mvc.project.mymvcproject.controller.socket.WsMessageController;
-
-import lombok.AllArgsConstructor;
+import com.my.mvc.project.mymvcproject.controller.socket.WsMessageHandler;
 
 @Configuration
-@AllArgsConstructor
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
-    WsMessageController wsMessageController;
+    @Lazy
+    @Autowired
+    WsMessageHandler wsMessageHandler;
+
+    @Bean
+    public List<WebSocketSession> webSocketSession() {
+        return new CopyOnWriteArrayList<>();
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(wsMessageController, "/socket");
-        registry.addHandler(wsMessageController, "/socket").withSockJS();
+        registry.addHandler(wsMessageHandler, "/socket");
+        registry.addHandler(wsMessageHandler, "/socket").withSockJS();
     }
 
 }
--- web-module/src/main/java/com/my/mvc/project/mymvcproject/controller/socket/WebSocketListener.java
+++ web-module/src/main/java/com/my/mvc/project/mymvcproject/controller/socket/WebSocketListener.java
@@ -2,11 +2,10 @@ package com.my.mvc.project.mymvcproject.controller.socket;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,7 +14,10 @@ import org.springframework.web.socket.WebSocketSession;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.my.mvc.project.mymvcproject.dto.EventDto;
+import com.my.mvc.project.mymvcproject.exceptions.UserNotRegisteredDetailsException;
+import com.my.mvc.project.mymvcproject.exceptions.UserNotValidException;
 import com.my.mvc.project.mymvcproject.model.Event;
+import com.my.mvc.project.mymvcproject.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,19 +26,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 class WebSocketListener {
-    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
-    private final ObjectMapper mapper;
-    // private final RequestContext requestContext;
+    protected final ObjectMapper mapper;
+    protected final UserService userService;
+    protected final List<WebSocketSession> sessions;
 
     @EventListener(Event.class)
-    @Scope(value = "singleton")
-    private void handleServerEvent(Event<String> event) {
-        if (event.getName().equals("subscription")) {
-            log.info("user has already subscribed and event is already handled");
-            return;
-        }
-        // TODO use user scoped sessions
-        getOpenWebSockets().forEach(webSocketSession -> {
+    private void handleServerEvent(Event<String> event)
+            throws UserNotValidException, UserNotRegisteredDetailsException {
+        var username = userService.getUserDetails(event.getReceiver().getId()).getUsername();
+        getUserOpenWebSockets(username).forEach(webSocketSession -> {
             var eventDto = EventDto.builder().id(event.getId()).name(event.getName()).data(event.getData()).build();
             try {
                 webSocketSession.sendMessage(new TextMessage(mapper.writeValueAsString(eventDto)));
@@ -54,15 +52,17 @@ class WebSocketListener {
         return openClosedSessions.get(true).stream();
     }
 
-    Stream<WebSocketSession> getUserOpenWebSockets() {
-        // TODO still is not user scoped
-        var openClosedSessions = sessions.stream().collect(Collectors.partitioningBy(WebSocketSession::isOpen));
+    Stream<WebSocketSession> getUserOpenWebSockets(String username) {
+        var predicate = (Predicate<WebSocketSession>) (session) -> session.getAttributes().get("contextUser").toString()
+                .equals(username);
+        var openClosedSessions = sessions.stream().filter(predicate)
+                .collect(Collectors.partitioningBy(WebSocketSession::isOpen));
         openClosedSessions.get(false).forEach(sessions::remove);
         log.info("clearing closed sessions before sending event");
         return openClosedSessions.get(true).stream();
     }
 
-    @Scheduled(fixedDelay = 1000000)
+    @Scheduled(fixedDelay = 864000)
     private void removeClosedSessions() {
         log.info("scheduled clearing the closed sessions");
         sessions.removeIf(webSocketSession -> !webSocketSession.isOpen());
